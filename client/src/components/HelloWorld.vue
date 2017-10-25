67df8f08e02c6efac4d25fcbe57c3a773e1e0412
Dalfin <geoffrey.dalfin@epsi.fr>
2017-10-25 22:24:50 +0200

    setup with vuetify

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,53 +1,33 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-card color="grey lighten-5" flat>
+    <v-toolbar dark color="primary" flat extended>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+    </v-toolbar>
+    <v-layout row>
+      <v-flex xs8 offset-xs2>
+        <v-card class="card--flex-toolbar">
+          <v-toolbar card color="white" prominent>
+            <v-toolbar-title class="body-2 grey--text">Title</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn icon>
+              <v-icon>search</v-icon>
+            </v-btn>
+            <v-btn icon>
+              <v-icon>apps</v-icon>
+            </v-btn>
+            <v-btn icon>
+              <v-icon>more_vert</v-icon>
+            </v-btn>
+          </v-toolbar>
+          <v-divider></v-divider>
+          <v-card-text style="height: 200px;"></v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-card>
 </template>
-
-<script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+<style>
+  .card--flex-toolbar {
+    margin-top: -64px;
   }
-}
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
