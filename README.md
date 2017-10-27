014ff2bd4146273044b075d17f89e1bd0a408f51
Dalfin <geoffrey.dalfin@epsi.fr>
2017-10-27 16:09:56 +0200

    clear README.md and getting started with Vuetify

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # TrouveMonBar
+
+**Version 1.0.0**
+
 Application pour trouver un bar
+## Getting started
+
+``` bash
+# Front-end
+cd client
+
+# Run the Client
+npm start dev
+
+# Back-end
+cd ../server
+
+# Build the Server
+npm start
+```
+
+## Contributors
+- Luc Nabhan
+- Geoffrey Dalfin
--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,53 @@
 <template>
-  <div id="app">
-    <!-- <img src="./assets/logo.png"> -->
-   <router-view/>
-  </div>
+ <v-app>
+  <v-toolbar dark color="primary">
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title class="white--text">Title</v-toolbar-title>
+    <v-spacer></v-spacer>
+        <v-flex xs8>
+            <v-text-field
+              v-on:keyup.enter="search"
+              v-model="searchField"
+              name="search"
+            ></v-text-field>
+          </v-flex>
+    <v-btn icon @click="search">
+      <v-icon>search</v-icon>
+    </v-btn>
+    <v-btn icon>
+      <v-icon>apps</v-icon>
+    </v-btn>
+    <v-btn icon>
+      <v-icon>refresh</v-icon>
+    </v-btn>
+    <v-btn icon>
+      <v-icon>more_vert</v-icon>
+    </v-btn>
+  </v-toolbar>
+  <main>
+    <v-content>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+  </main>
+  <v-footer app></v-footer>
+</v-app>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      searchField: ''
+    }
+  },
+  methods: {
+    search () {
+      console.log(this.searchField)
+    }
+  }
 }
 </script>
 
